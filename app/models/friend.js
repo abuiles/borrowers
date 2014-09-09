a47b4732afc6a6ec2039325f735b069fcba4ea1d
Adolfo Builes <builes.adolfo@gmail.com>
2014-09-09 12:38:20 -0500

    Add fullName CP.

--- app/models/friend.js
+++ app/models/friend.js
@@ -5,5 +5,8 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
-  totalArticles: DS.attr('number')
+  totalArticles: DS.attr('number'),
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return this.get('firstName') + this.get('lastName');
+  })
 });
