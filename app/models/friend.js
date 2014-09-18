9f3f165a6bb873b5e8e01ce8ece335ed35d9e936
Adolfo Builes <builes.adolfo@gmail.com>
2014-09-18 12:12:14 -0500

    Add article model

--- app/models/friend.js
+++ app/models/friend.js
@@ -2,11 +2,12 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string'),
-  twitter: DS.attr('string'),
+  articles:      DS.hasMany('articles'),
+  email:         DS.attr('string'),
+  firstName:     DS.attr('string'),
+  lastName:      DS.attr('string'),
   totalArticles: DS.attr('number'),
+  twitter:       DS.attr('string'),
   fullName: Ember.computed('firstName', 'lastName', function() {
     return this.get('firstName') + ' ' + this.get('lastName');
   })
