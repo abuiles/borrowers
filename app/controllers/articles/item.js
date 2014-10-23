018b245d9a2180b99bf02b08c5a54fec075d8eda
Adolfo Builes <builes.adolfo@gmail.com>
2014-10-23 09:35:12 -0500

    Add ember-computed-change-gate.

--- app/controllers/articles/item.js
+++ app/controllers/articles/item.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   states: ['borrowed', 'returned'],
   autoSave: function() {
-    this.send('save', this.get('model'));
+    if (!this.get('isNew')) {
+      this.send('save', this.get('model'));
+    }
   },
   isDirtyChanged: function() {
     if (this.get('isDirty') && !this.get('isSaving')) {
--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('friends/show').get('articles');
+    var articles = this.modelFor('friends/show').get('articles');
+
+    if (articles.get('isFulfilled')) {
+      articles.reload();
+    }
+
+    return articles;
   },
   actions: {
     save: function(model) {
