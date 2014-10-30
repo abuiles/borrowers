50abcc09dba98e417ec927f5e75d14270f60e87b
Adolfo Builes <builes.adolfo@gmail.com>
2014-10-30 09:10:34 -0500

    Put fonts under assets/fonts.

--- Brocfile.js
+++ Brocfile.js
@@ -2,20 +2,24 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  fingerprint: {
+    prepend: 'https://d29sqib8gy.cloudfront.net/'
+  },
+});
 
 app.import('vendor/fontello/fontello.css');
 app.import('vendor/fontello/font/fontello.ttf', {
-  destDir: 'font'
+  destDir: 'assets/fonts'
 });
 app.import('vendor/fontello/font/fontello.eot', {
-  destDir: 'font'
+  destDir: 'assets/fonts'
 });
 app.import('vendor/fontello/font/fontello.svg', {
-  destDir: 'font'
+  destDir: 'assets/fonts'
 });
 app.import('vendor/fontello/font/fontello.woff', {
-  destDir: 'font'
+  destDir: 'assets/fonts'
 });
 app.import('bower_components/picnic/releases/latest.min.css');
 app.import('bower_components/moment/moment.js');
