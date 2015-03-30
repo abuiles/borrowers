cc8a97cc9a541a403193631a0608ef2b83ec77b4
Adolfo Builes <builes.adolfo@gmail.com>
2015-03-30 11:17:16 -0500

    Add picnic and update ember-cli.

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['show'],
   possibleStates: ["borrowed", "returned"],
-  contentDidChange: function() {
+  contentDidChange: Ember.observer('model.[]', function() {
     console.log('Called when we add or removed an article.');
-  }.observes('model.[]'),
-  stateDidChange: function() {
+  }),
+  stateDidChange: Ember.observer('model.@each.state', function() {
     console.log('Called when the state property change for any of the articles.');
-  }.observes('model.@each.state')
+  })
 });
