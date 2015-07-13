bd3b7766cc9dd939fc699b0d7c25dd1413a1039a
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-13 10:13:10 -0500

    Section: Hands-on, creating our first Route.

--- app/router.js
+++ app/router.js
@@ -6,7 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('friends');
+  this.route('friends', function() {
+  });
 });
 
 export default Router;
---
+++ app/routes/friends/index.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  //
+  // Here we are using ES6 syntax for functions!
+  // We can use this out of the box with ember-cli
+  // thanks to the addon ember-cli-babel
+  //
+  // To learn more about es6, check http://s.abuiles.com/bwWo
+  //
+  model() {
+    return this.store.findAll('friend');
+  }
+});
