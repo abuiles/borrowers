229ee790b75f2e4a3751a2ca322c076db8dda760
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-14 09:17:39 -0500

    Section: Hands-on, add friend form.

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,17 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.email')) &&
+          !Ember.isEmpty(this.get('model.firstName')) &&
+          !Ember.isEmpty(this.get('model.lastName')) &&
+          !Ember.isEmpty(this.get('model.twitter'));
+      }
+    }
+  ),
   actions: {
     save() {
-      console.log('+- save action in friends new controller');
+      if (this.get('isValid')) {
+        this.get('model').save().then((friend) => {
+          this.transitionToRoute('friends/show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
 
-      return true;
+      return false;
     },
     cancel() {
-      console.log('+- cancel action in friends new controller');
+      this.transitionToRoute('friends');
 
-      return true;
-
+      return false;
     }
   }
 });
