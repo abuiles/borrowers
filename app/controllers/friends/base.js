6e11557241f0d6cf7bdb2fdb0eada5863a88ef73
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-14 09:42:58 -0500

    Section: hands-on, refactoring.

---
+++ app/controllers/friends/base.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.email')) &&
+          !Ember.isEmpty(this.get('model.firstName')) &&
+          !Ember.isEmpty(this.get('model.lastName')) &&
+          !Ember.isEmpty(this.get('model.twitter'));
+      }
+    }
+  ),
+  actions: {
+    save() {
+      if (this.get('isValid')) {
+        this.get('model').save().then((friend) => {
+          this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+
+      return false;
+    },
+    cancel() {
+      return true;
+    }
+  }
+});
--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -1,32 +1,7 @@
-import Ember from 'ember';
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    {
-      get() {
-        return !Ember.isEmpty(this.get('model.email')) &&
-          !Ember.isEmpty(this.get('model.firstName')) &&
-          !Ember.isEmpty(this.get('model.lastName')) &&
-          !Ember.isEmpty(this.get('model.twitter'));
-      }
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save() {
-      if (this.get('isValid')) {
-        this.get('model').save().then((friend) => {
-          this.transitionToRoute('friends.show', friend);
-        });
-      } else {
-        this.set('errorMessage', 'You have to fill all the fields');
-      }
-
-      return false;
-    },
     cancel() {
       this.transitionToRoute('friends.show', this.get('model'));
 
