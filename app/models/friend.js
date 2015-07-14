7afd2c2e718fa9ed969cae62a1352f52b5762533
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-14 09:31:08 -0500

    Section: Hands-on, Visiting friends.

--- app/models/friend.js
+++ app/models/friend.js
@@ -5,5 +5,10 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
-  totalArticles: DS.attr('number')
+  totalArticles: DS.attr('number'),
+  fullName: Ember.computed('firstName', 'lastName', {
+    get() {
+      return this.get('firstName') + ' ' + this.get('lastName');
+    }
+  })
 });
