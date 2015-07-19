8eb6ceaf08f2b96d30664109981f4b4949d6206b
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-18 19:01:49 -0500

    Section: Hands-on, use computed property macros.

--- app/controllers/friends/base.js
+++ app/controllers/friends/base.js
@@ -1,19 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    {
-      get() {
-        return !Ember.isEmpty(this.get('model.email')) &&
-          !Ember.isEmpty(this.get('model.firstName')) &&
-          !Ember.isEmpty(this.get('model.lastName')) &&
-          !Ember.isEmpty(this.get('model.twitter'));
-      }
-    }
+  hasEmail:     Ember.computed.notEmpty('model.email'),
+  hasFirstName: Ember.computed.notEmpty('model.firstName'),
+  hasLastName:  Ember.computed.notEmpty('model.lastName'),
+  hasTwitter:   Ember.computed.notEmpty('model.twitter'),
+  isValid:      Ember.computed.and(
+    'hasEmail',
+    'hasFirstName',
+    'hasLastName',
+    'hasTwitter'
   ),
   actions: {
     save() {
