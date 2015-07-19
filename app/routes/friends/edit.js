c3ece0eb83e1c4803fa710ae364f373ccc8524fd
Adolfo Builes <builes.adolfo@gmail.com>
2015-07-18 20:36:19 -0500

    Section: Hands-on, route hooks.

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      var model = controller.get('model');
+      model.rollback();
+    }
+  }
 });
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,29 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
   },
+  activate() {
+    console.log('----- activate hook called -----');
+  },
+  deactivate() {
+    console.log('----- deactivate hook called -----');
+  },
+  resetController: function (controller, isExiting, transition) {
+    if (isExiting) {
+      // We grab the model from the controller
+      //
+      var model = controller.get('model');
+
+      // If we are leaving the Route we verify if the model is in
+      // 'isNew' state, which means it wasn't saved to the backend.
+      //
+      if (model.get('isNew')) {
+
+        // We call DS#destroyRecord() which removes it from the store
+        //
+        model.destroyRecord();
+      }
+    }
+  },
   actions: {
     save() {
       console.log('+-- save action bubbled up to friends new route');
