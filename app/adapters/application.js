eaed661ac50b1327be917bb2f22d614fb8434218
Oskar Niburski <oskarniburski@gmail.com>
2016-06-05 02:11:40 -0400

    Update adapater host and index request

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   namespace: 'api/v4',
+  host: 'http://api.ember-cli-101.com',
   coalesceFindRequests: true
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import request from 'ic-ajax';
 
 export default Ember.Route.extend({
   model()  {
-    return request('/api/friends').then(function(data){
+    return request('http://api.ember-cli-101.com/api/v4/friends').then(function(data) {
       return {
         friendsCount: data.friends.length
       };
