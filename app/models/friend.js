651841bb44e4a1f113f6630e4f0654a1ada7ffe0
Oskar Niburski <oskarniburski@gmail.com>
2016-06-05 02:28:44 -0400

    Change gate addition

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import changeGate from 'ember-computed-change-gate/change-gate';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
@@ -12,5 +13,8 @@ export default DS.Model.extend({
     get() {
       return this.get('firstName') + ' ' + this.get('lastName');
     }
+  }),
+  capitalizedFirstName: changeGate('firstName', function(firstName) {
+    return Ember.String.capitalize(firstName); 
   })
 });
